40b9abe9b930d4ec79baf18e4bd54952c789c399
Garrett <grolemund@gmail.com>
2012-09-03 18:54:09 -0500

    Removed unnecessary data help files

--- R/data.r
+++ R/data.r
@@ -4,24 +4,6 @@
 #' @keywords data
 NULL
 
-#' glyph-map monthly temperature prediction data 
-#' @name seasons
-#' @docType data
-#' @keywords data
-NULL
-
-#' glyph-map yearly temperature prediction data 
-#' @name years
-#' @docType data
-#' @keywords data
-NULL
-
-#' modified version of swiss data
-#' @name test.data
-#' @docType data
-#' @keywords data
-NULL
-
 #' casualty data from the Afghan War Diary, lightly cleaned
 #' @name casualties
 #' @docType data
