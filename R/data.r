8788689c4823f5aa2fb8aea976e26eeaa84e896a
hadley <h.wickham@gmail.com>
2015-07-09 06:24:37 -0700

    Improve data docs

--- R/data.r
+++ R/data.r
@@ -1,16 +1,10 @@
 #' Nasa data from the 2006 American Statistical Association Data Expo
-#' @name nasa
-#' @docType data
-#' @keywords data
-NULL
+"nasa"
 
 #' casualty data from the Afghan War Diary, lightly cleaned
 #'
-#' The Afghan War Diary is distributed by the WikiLeaks organization and available 
-#' for download at http://www.wikileaks.org/wiki/Afghan_War_Diary,_2004-2010. Each 
-#' row of the data set corresponds to a single casualty 
-#' (one person being injured or killed). 
-#' @name casualties
-#' @docType data
-#' @keywords data
-NULL
+#' The Afghan War Diary is distributed by the WikiLeaks organization and available
+#' for download at http://www.wikileaks.org/wiki/Afghan_War_Diary,_2004-2010. Each
+#' row of the data set corresponds to a single casualty
+#' (one person being injured or killed).
+"casualties"
